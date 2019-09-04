847e2568cf630f798372ef0c0a2387f460526bdb
Helsis <seagull.bogdan@gmail.com>
2019-09-04 17:51:06 +0300

    feat( config ): add configuration module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 
+import { ConfigModule } from '../config/config.module';
+
 @Module({
-  imports: [],
+  imports: [ConfigModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app/app.module';
+import { ConfigService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = await new ConfigService();
+
   return await app
+    .setGlobalPrefix(config.get('API_PREFFIX'))
     .useGlobalPipes(new ValidationPipe())
     .enableCors()
-    .listen(3000);
+    .listen(config.get('API_PORT'));
 }
 bootstrap();
