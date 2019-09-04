0aaf65111a16317161cd3d8ba58ac3cc3918731a
Helsis <seagull.bogdan@gmail.com>
2019-09-04 18:03:00 +0300

    feat( open API ): add Swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 import { ConfigService } from './config/config.service';
@@ -7,6 +8,15 @@ import { ConfigService } from './config/config.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = await new ConfigService();
+  const options = await new DocumentBuilder()
+    .setDescription(config.get('npm_package_description'))
+    .setVersion(config.get('npm_package_version'))
+    .setTitle(config.get('npm_package_name'))
+    .setBasePath(config.get('API_PREFFIX'))
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('', app, document);
 
   return await app
     .setGlobalPrefix(config.get('API_PREFFIX'))
