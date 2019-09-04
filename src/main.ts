c1f4627f97cf348b6c10eef1e9bf3967ffccd375
Helsis <seagull.bogdan@gmail.com>
2019-09-04 18:12:50 +0300

    feat( compodoc ): add auto generate documentation

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import { join } from 'path';
 
 import { AppModule } from './app/app.module';
 import { ConfigService } from './config/config.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const config = await new ConfigService();
   const options = await new DocumentBuilder()
     .setDescription(config.get('npm_package_description'))
@@ -19,9 +22,14 @@ async function bootstrap() {
   SwaggerModule.setup('', app, document);
 
   return await app
+    .useStaticAssets(join(__dirname, '..', 'public'), {
+      prefix: config.get('DOC_PREFFIX'),
+    })
+    .setBaseViewsDir(join(__dirname, '..', 'public'))
     .setGlobalPrefix(config.get('API_PREFFIX'))
     .useGlobalPipes(new ValidationPipe())
     .enableCors()
     .listen(config.get('API_PORT'));
 }
+
 bootstrap();
